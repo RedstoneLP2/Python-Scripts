e67520c3c2cb43ad15a82a36a4e26f43c0e002a5
RedstoneLP2 <42359960+RedstoneLP2@users.noreply.github.com>
2022-05-21 20:57:23 +0200

    modified to not use api directly

--- FTBConverter.py
+++ FTBConverter.py
@@ -6,27 +6,31 @@
 
 
 import zipfile
-import urllib.request
 import os
 import requests
 import tempfile
 import json
 import sys
 import pathlib
+import hashlib
+import shutil
 
-
-apiurl = "https://api.modpacks.ch/public/modpack/"
 forge = False
 
 if sys.version_info[0] < 3:
     raise Exception("Python 3 or a more recent version is required.")
 
-from urllib.parse import urlparse
-
 def files(path):
 	for file in os.listdir(path):
 		if os.path.isfile(os.path.join(path, file)):
 			yield file
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def download(url, filename): #fancy download
 	with open(filename, 'wb') as f:
@@ -49,54 +53,6 @@ def download(url, filename): #fancy download
 				sys.stdout.flush()
 	sys.stdout.write('\n')
 
-
-
-def apisearch(searchterm):
-
-	req = urllib.request.Request(apiurl+"search/10?term="+searchterm) # Generate Request for first 10 results
-
-	if urllib.request.urlopen(req).getcode()==404:
-		print("Error 404 while accessing API. Please check URL")
-		sys.exit(1)
-
-	apiresults = urllib.request.urlopen(req).read()
-	rawResults = json.loads(apiresults)
-	
-
-	if "status" in rawResults and rawResults["status"] == "error":
-		print(searchterm +" not found")
-		sys.exit(1)
-
-	mappedNames = {}
-
-	for packid in rawResults["packs"]:
-		mappedNames[getmeta(packid)["name"]] = packid
-
-	return rawResults, mappedNames
-
-
-def getmeta(packid):
-	reqm = urllib.request.Request(apiurl+str(packid)) # Generate Request
-	if urllib.request.urlopen(reqm).getcode()==404:
-		print("Error 404 while accessing API. Please check Connection")
-		sys.exit(1)
-
-	packmeta = urllib.request.urlopen(reqm).read()
-	pmeta = json.loads(packmeta)
-	return pmeta
-
-
-def apiparse(packid, verid):
-	req = urllib.request.Request(apiurl+packid+"/"+verid) # Generate Request
-	if urllib.request.urlopen(req).getcode()==404:
-		print("Error 404 while accessing API. Please check Connection")
-		sys.exit(1)
-
-	packinfo = urllib.request.urlopen(req).read()
-	pinf = json.loads(packinfo)
-	return pinf, getmeta(packid)
-
-
 def zipfolder(foldername, target_dir): #def for zipping folders
 	zipobj = zipfile.ZipFile(foldername + '.zip', 'w', zipfile.ZIP_DEFLATED)
 	rootlen = len(target_dir) + 1
@@ -107,95 +63,46 @@ def zipfolder(foldername, target_dir): #def for zipping folders
 
 with tempfile.TemporaryDirectory() as tempDir: #create tempDir
 
+	packpath = input('Please Enter Path to FTB Instance folder: ')
+	pinf = json.load(open(os.path.join(packpath,"instance.json"),"r"))
+
 	packDir = os.path.join(tempDir, "modpack")
 	pDir = os.path.join(packDir, "modpack")
 	mmcFile = os.path.join(pDir, "mmc-pack.json")
 	minecraft = os.path.join(pDir, "minecraft")
 
 	os.makedirs(pDir)
-	os.mkdir(minecraft)
+	#os.mkdir(minecraft)
 
-	usersearch = input('Enter Searchterm: ')
-	mappedNames = apisearch(usersearch)[1]
-		
-	print(os.linesep + "ID ---- Name")
-	for pack in mappedNames:
-		print(str(mappedNames[pack]) + " ---- " + pack)
+	mcver = pinf["mcVersion"]
+	if "forge" in pinf["modLoader"].lower():
+		forge = True
+		loaderver = pinf["modLoader"].split("-")[2]
+	elif "fabric" in pinf["modLoader"].lower():
+		fabric = True
+		loaderver = pinf["modLoader"].split("-")[3]
+
+	shutil.copytree(packpath,minecraft)
+
+	artUrl=pinf["artUrl"]
+	iconFileName=os.path.join(pDir,artUrl[artUrl.rfind("/")+1:].split("?", 1)[0])
+	download(artUrl,iconFileName)
 	
-	userInput = input('Enter ID of Pack: ')
-	
-	for pack in mappedNames:
-		found = False
-		if str(mappedNames[pack]) == str(userInput):
-			pmeta = getmeta(str(userInput))
-			found = True
-			break
-
-	if not found:
-		print("PackID "+ str(userInput) + " not found")
-		sys.exit(1)
-	PackID = userInput
-
-	print(os.linesep + "ID ---- Version ---- Type")
-	for ver in pmeta["versions"]:
-
-		print(str(ver["id"]) + " ---- " + str(ver["name"]) + " ---- " + str(ver["type"]))
-	
-	userInput = input('Enter ID of Pack Version: ')
-
-
-	for ver in pmeta["versions"]:
-		found = False
-		if str(ver["id"]) == str(userInput):
-			found = True
-			break
-
-	if not found:
-		print("Pack Version "+ str(userInput) + " not found")
-		sys.exit(1)
-	verid = userInput
-
-
-
-	pinf, pmeta = apiparse(PackID,verid)
-	
-	for n in pinf["targets"]:
-		if n["name"] == "minecraft":
-			mcver = n["version"]
-			break
-		if n["name"] == "forge":
-			forge = True
-			forgever = n["version"]
-
-
-	for n in pinf["files"]:
-		zipurl = n["url"]
-		zname = pathlib.PurePath(minecraft).joinpath(n["path"]).joinpath(n["name"])
-		pathlib.Path(minecraft).joinpath(n["path"]).mkdir(parents=True,exist_ok=True)
-		
-		print(zipurl)
-		download(zipurl, zname)
-		
-
-	iconUrl = "None"
-	for entry in pmeta["art"]:
-		if entry["type"] == "square":
-			iconUrl=entry["url"]
-
-	iconName=iconUrl[iconUrl.rfind("/")+1:].split("?", 1)[0]
+	iconName=md5(iconFileName)+"."+iconFileName.split(".")[-1]
+	os.rename(iconFileName, os.path.join(pDir,iconName))
 	print(iconName)
 
-	download(iconUrl, os.path.join(pDir, iconName))
-
 	if forge:
-		mmcData = '''{"components": [{"cachedName": "Minecraft","cachedRequires": [],"cachedVersion": "'''+mcver+'''","important": true,"uid": "net.minecraft","version": "'''+mcver+'''"},{"cachedName": "Forge","cachedRequires": [{"equals": "'''+mcver+'''","uid": "net.minecraft"}],"cachedVersion": "'''+forgever+'''","uid": "net.minecraftforge","version": "'''+forgever+'''"}],"formatVersion": 1}'''
+		mmcData = '''{"components": [{"cachedName": "Minecraft","cachedRequires": [],"cachedVersion": "'''+mcver+'''","important": true,"uid": "net.minecraft","version": "'''+mcver+'''"},{"cachedName": "Forge","cachedRequires": [{"equals": "'''+mcver+'''","uid": "net.minecraft"}],"cachedVersion": "'''+loaderver+'''","uid": "net.minecraftforge","version": "'''+loaderver+'''"}],"formatVersion": 1}'''
+	elif fabric:
+		mmcData = '''{"components": [{"cachedName": "Minecraft","cachedRequires": [],"cachedVersion": "'''+mcver+'''","important": true,"uid": "net.minecraft","version": "'''+mcver+'''"},{"cachedName": "Intermediary Mappings","cachedRequires": [{"equals": "'''+mcver+'''","uid": "net.minecraft"}],"cachedVersion": "'''+mcver+'''","cachedVolatile": true,"DependencyOnly": true,"uid": "net.fabricmc.intermediary","version": "'''+mcver+'''"},{"cachedName": "Fabric Loader","cachedRequires": [{"uid": "net.fabricmc.intermediary"}],"cachedVersion": "'''+loaderver+'''","uid": "net.fabricmc.fabric-loader","version": "'''+loaderver+'''"}],"formatVersion": 1}'''
 	else:
 		mmcData = '''{"components": [{"cachedName": "Minecraft","cachedRequires": [],"cachedVersion": "'''+mcver+'''","important": true,"uid": "net.minecraft","version": "'''+mcver+'''"}],"formatVersion": 1}'''
 
 	instancecfg = '''InstanceType=OneSix
 	MCLaunchMethod=LauncherPart
-	name='''+pmeta["name"]+'''
-	notes='''+pmeta["description"]+'''
+	name='''+pinf["name"]+" "+pinf["version"]+'''
+	notes='''+pinf["description"]+'''
 	iconKey='''+iconName.replace(".png", "")
 
 
@@ -207,6 +114,6 @@ with tempfile.TemporaryDirectory() as tempDir: #create tempDir
 	with open(os.path.join(pDir,"instance.cfg"), "w+") as instanceFile: # write instancecfg in instance.cfg
 		instanceFile.write(instancecfg)
 		instanceFile.close()
-
-	zipfolder(pmeta["name"].replace(" ", "_").replace("/", "").replace("\\", ""), packDir) # zip everything up
-	print("Output File: "+pmeta["name"].replace(" ", "_").replace("/", "").replace("\\", "")+".zip")
+	outputFileName=pinf["name"].replace(" ", "_").replace("/", "").replace("\\", "")+"-"+pinf["version"]
+	zipfolder(outputFileName, packDir) # zip everything up
+	print("Output File: "+outputFileName+".zip")
